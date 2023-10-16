18eeba9f11d065be00b484c4b01444bd5ffffd9b
AestridJiujian <AestridJiujian@users.noreply.github.com>
2023-10-16 09:36:14 -0300

    DONE

---
+++ filtrado.r
@@ -0,0 +1,8 @@
+# Generate csv with filtered observations.
+
+library(tidyverse)
+df <- read.csv("coc_clans.csv")
+filtered_df <- df %>% filter(num_members >= 5)
+filtered_df <- subset(df, select = -c(clan_description, clan_tag, clan_name, clan_badge_url))
+
+write.csv(filtered_df, "coc_filtrado.csv")
